0ee8b3889402613c7db3e9ee6b771378889e69dc
Adrielli-Bertoldo <adrielli.bertoldo@gmail.com>
2025-07-07 12:06:01 -0300

    Montando deploy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Dri')
+  .setDescription('Projeto Blog Dri')
+  .setContact("Adrielli Bertoldo","https://github.com/Adrielli-Bertoldo","bertoldoadrielli@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '3:00'
 
